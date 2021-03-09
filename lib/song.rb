b015b21329032368aed100849b01d77352b76be3
Jesse Wenger <wengerj@gmail.com>
2021-03-09 17:56:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
     song = Song.new(song_name)
-    artist_name(artist_name)
+    artist_name=(artist_name)
     
   end
   
