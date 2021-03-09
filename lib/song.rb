2f90641630baa81ce6482ab1db9f5f5a7ea043a7
Jesse Wenger <wengerj@gmail.com>
2021-03-09 17:33:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,20 +13,6 @@ class Song
   end
   
   def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-=begin
-    if (song.artist.nil?)
-      song.artist = Artist.new(artist_name)
-    else
-      song.artist.name = artist_name
-    end
-=end
+
   end
-  
-  def artist_name=(artist_name)
-    
-  end
-  
 end
